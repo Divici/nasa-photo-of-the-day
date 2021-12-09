53f5755bd5d70c88a43b5c88884f1fdd326a16e2
david-aihe <doa9200@gmail.com>
2021-12-09 15:45:43 -0500

    Added styled compoonents to explanation

--- src/Explanation.js
+++ src/Explanation.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledExplanation = styled.div`
+    width: 80%;
+    display: flex;
+    margin: 0 auto 5%;
+    background-color: lightgrey;
+    p {
+        padding: 2% 10%;
+    }
+`;
 
 const Explanation = props =>{
 
     const {explanation} = props;
 
     return (
-        <div>
+        <StyledExplanation>
             <p>{explanation}</p>
-        </div>
+        </StyledExplanation>
     )
 }
 
